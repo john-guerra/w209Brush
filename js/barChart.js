a8679bd0b7d9ccf181aeb50c1eb719b6006e550f
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-10-03 21:29:11 -0500

    Forgot css

--- js/barChart.js
+++ js/barChart.js
@@ -1,22 +1,27 @@
 /* global d3 */
 
 function barChart() {
-
-  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+  var margin = { top: 20, right: 20, bottom: 30, left: 40 },
     width = 400,
     height = 400,
     innerWidth = width - margin.left - margin.right,
     innerHeight = height - margin.top - margin.bottom,
-    xValue = function(d) { return d[0]; },
-    yValue = function(d) { return d[1]; },
+    xValue = function(d) {
+      return d[0];
+    },
+    yValue = function(d) {
+      return d[1];
+    },
     xScale = d3.scaleBand().padding(0.1),
     yScale = d3.scaleLinear();
 
   function chart(selection) {
-    selection.each(function (data) {
-
+    selection.each(function(data) {
       // Select the svg element, if it exists.
-      var svg = d3.select(this).selectAll("svg").data([data]);
+      var svg = d3
+        .select(this)
+        .selectAll("svg")
+        .data([data]);
 
       // Otherwise, create the skeletal chart.
       var svgEnter = svg.enter().append("svg");
@@ -25,49 +30,63 @@ function barChart() {
       gEnter.append("g").attr("class", "y axis");
 
       // Update the outer dimensions.
-      svg.merge(svgEnter).attr("width", width)
+      svg
+        .merge(svgEnter)
+        .attr("width", width)
         .attr("height", height);
 
       // Update the inner dimensions.
-      var g = svg.merge(svgEnter).select("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      var g = svg
+        .merge(svgEnter)
+        .select("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
-      xScale.rangeRound([0, innerWidth])
-        .domain(data.map(function(d) { return xValue(d); }));
-      yScale.rangeRound([innerHeight, 0])
-        .domain([0, d3.max(data, function(d) { return yValue(d); })]);
+      xScale.rangeRound([0, innerWidth]).domain(
+        data.map(function(d) {
+          return xValue(d);
+        })
+      );
+      yScale.rangeRound([innerHeight, 0]).domain([
+        0,
+        d3.max(data, function(d) {
+          return yValue(d);
+        })
+      ]);
 
       g.select(".x.axis")
-          .attr("transform", "translate(0," + innerHeight + ")")
-          .call(d3.axisBottom(xScale));
+        .attr("transform", "translate(0," + innerHeight + ")")
+        .call(d3.axisBottom(xScale));
 
       g.select(".y.axis")
-          .call(d3.axisLeft(yScale).ticks(10, "%"))
+        .call(d3.axisLeft(yScale))
         .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", "0.71em")
-          .attr("text-anchor", "end")
-          .text("Frequency");
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Frequency");
 
-      var bars = g.selectAll(".bar")
-        .data(function (d) { return d; });
+      var bars = g.selectAll(".bar").data(function(d) {
+        return d;
+      });
 
-      bars.enter().append("rect")
-          .attr("class", "bar")
+      bars
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
         .merge(bars)
-          .attr("x", X)
-          .attr("y", Y)
-          .attr("width", xScale.bandwidth())
-          .attr("height", function(d) { return innerHeight - Y(d); });
+        .attr("x", X)
+        .attr("y", Y)
+        .attr("width", xScale.bandwidth())
+        .attr("height", function(d) {
+          return innerHeight - Y(d);
+        });
 
       bars.exit().remove();
     });
-
   }
 
-// The x-accessor for the path generator; xScale ∘ xValue.
+  // The x-accessor for the path generator; xScale ∘ xValue.
   function X(d) {
     return xScale(xValue(d));
   }
@@ -109,6 +128,3 @@ function barChart() {
 
   return chart;
 }
-
-
-
--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-/* global d3 */
+/* global d3, barChart */
 
 console.log("works!", d3);
 
+var bc = barChart()
+  .x(function(d) {
+    return d.x;
+  })
+  .y(function(d) {
+    return d.y;
+  });
 
-d3.csv("./Lekagul_slice.csv", function (err, data) {
+d3.csv("./Lekagul_slice.csv", function(err, data) {
   if (err) throw err;
 
   console.log("Got data", data[0]);
@@ -15,6 +22,13 @@ d3.csv("./Lekagul_slice.csv", function (err, data) {
     }
   }
 
-  console.log("entrance 3 =" , count);
+  d3.select("#gates")
+    .datum([
+      { x: "uno", y: 1 }, //Fake data
+      { x: "dos", y: 5 },
+      { x: "tres", y: 3 }
+    ])
+    .call(bc);
 
+  console.log("entrance 3 =", count);
 });
