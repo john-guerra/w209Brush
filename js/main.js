c2dd868bad8aa139c7bcc1df52b8f6f015eb6875
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-10-03 21:15:21 -0500

    counting

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 /* global d3 */
 
 console.log("works!", d3);
+
+
+d3.csv("./Lekagul_slice.csv", function (err, data) {
+  if (err) throw err;
+
+  console.log("Got data", data[0]);
+
+  var count = 0;
+  for (var row of data) {
+    if (row["gate-name"] === "entrance3") {
+      count++;
+    }
+  }
+
+  console.log("entrance 3 =" , count);
+
+});
